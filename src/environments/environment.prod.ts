5856ba8487e52eb5e4c0d25891aa539cebd98a8e
Gourav More <gourav_m@tekditechnologies.com>
2024-01-29 18:17:50 +0530

    Issue #212868 fix: added configure github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  port: 'https://www.learnerai-dev.theall.ai/content-service/',
+	port1 :'http://localhost:3008/',
+	configPort : "http://localhost:3000/"
 };
